e86cceb15166b88f0f9253346c147ed4d1ceb57d
Vinicius Janner <viniciusjanner@gmail.com>
2022-08-16 13:56:16 -0300

    Files project.

---
+++ app/src/main/java/com/viniciusjanner/app/camerax/pinch/zoom/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.viniciusjanner.app.camerax.pinch.zoom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
